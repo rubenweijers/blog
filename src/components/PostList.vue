e0ff478221302d9cbad5928ae44cbb178255832c
ruben <rubenweijers29@gmail.com>
2024-05-09 18:40:37 +0200

    Blog

---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div v-for="post in posts" :key="post.id">
+     <SinglePost :post="post"/>
+    </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from 'vue'
+import SinglePost from './SinglePost.vue'
+export default {
+    props: ['posts'],
+    components: { SinglePost },
+    setup(props) {
+        onMounted(() => console.log('mounted'))
+        onUnmounted(() => console.log('unmounted'))
+        onUpdated(() => console.log('updated'))
+    },
+
+}
+
+</script>
+
+
+<style>
+
+</style>
